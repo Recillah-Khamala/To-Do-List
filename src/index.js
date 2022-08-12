8c1d93cb906156bcd7905750bbefd7120610f450
Recillah Khamala <recillahk@gmail.com>
2022-08-12 20:29:36 +0300

    Add assignent files and code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
-import _ from 'lodash';
 import './style.css';
-import printMe from './print.js';
 
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button')
+const toDoTasks = [
+  {
+    description: 'Cleaned the house',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Prepared all the meals',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Done weekly shopping',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Finished the assignment',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Completed the daily workout',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Cleaned the car',
+    completed: false,
+    index: 1,
+  },
+];
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    btn.innerHTML = 'click me and check the console';
-    btn.onclick = printMe
-
-    return element;
-}
-  
-  document.body.appendChild(component());
+toDoTasks.forEach((element) => {
+  const wholeList = document.querySelector('.chores');
+  const choreList = document.createElement('div');
+  choreList.classList.add('choreDetails');
+  choreList.innerHTML = `<div class="oneChore">
+    <input type="checkbox">
+    <h2>${element.description}</h2>
+   </div>
+   <div class="icon">
+   <i class="fa-solid fa-ellipsis-vertical"></i>
+   </div>`;
+  wholeList.appendChild(choreList);
+});
